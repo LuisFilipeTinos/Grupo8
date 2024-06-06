197ca77cd9a220235dc30d5dbda20b4619b1ed50
Luis Filipe Tinos <lftinos@gmail.com>
2024-06-06 16:23:33 -0300

    Ajuste de drop

--- Assets/Scripts/CollisionControllerMateriaisVidro.cs
+++ Assets/Scripts/CollisionControllerMateriaisVidro.cs
@@ -6,27 +6,26 @@ using UnityEngine;
 
 public class CollisionControllerMateriaisVidro : MonoBehaviour
 {
-  
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
 
-               switch (collision.gameObject.tag)
-                {
-                    case "Vidro":
-                        Destroy(gameObject);
-                        break;
-                    case "Metal":
-                        Destroy(gameObject);
-                        //Link para aba de perca de pontos
-                        break;
-                    case "Plastico":
-                        Destroy(gameObject);
-                        //Link para aba de perca de pontos
-                        break;
-                    case "Papel":
-                        Destroy(gameObject);
-                        //Link para aba de perca de pontos
-                        break;
-                }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Vidro":
+                Destroy(gameObject);
+                break;
+            case "Metal":
+                Destroy(gameObject);
+                //Link para aba de perca de pontos
+                break;
+            case "Plastico":
+                Destroy(gameObject);
+                //Link para aba de perca de pontos
+                break;
+            case "Papel":
+                Destroy(gameObject);
+                //Link para aba de perca de pontos
+                break;
         }
+    }
 }
